9f0904f676b7b1bb5f3141936b4f980e66cd435e
Daniel <dsanjuan1999@gmail.com>
2025-11-24 17:36:38 -0300

    Se crea archivos base para el proyecto (feature para modulos de lab y usuario) y models (para el futuro consumo de ms usuario y lab)

---
+++ laboratorio-clinico/src/app/features/laboratorios/lab-form/lab-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-form',
+  imports: [],
+  templateUrl: './lab-form.component.html',
+  styleUrl: './lab-form.component.css'
+})
+export class LabFormComponent {
+
+}
---
+++ laboratorio-clinico/src/app/features/laboratorios/lab-list/lab-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-list',
+  imports: [],
+  templateUrl: './lab-list.component.html',
+  styleUrl: './lab-list.component.css'
+})
+export class LabListComponent {
+
+}
